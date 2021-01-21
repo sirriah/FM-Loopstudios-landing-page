0972081e5cbfd3c3c037ae0cdaf5f0573aa02546
sirriah <tereza.dvorakova.3@seznam.cz>
2021-01-21 23:35:00 +0100

    add mobile menu funcitonality and hero design

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('open hamburger');
+    
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else {
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
