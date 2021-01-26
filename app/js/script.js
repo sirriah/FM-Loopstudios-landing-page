695010fd21e0da9e7b2baa99868206803c1c43c4
sirriah <tereza.dvorakova.3@seznam.cz>
2021-01-26 23:17:25 +0100

    add some comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,7 @@
+/*
+Script for toggle mobile menu
+*/
+
 const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
